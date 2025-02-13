c746563aa8a75f786f852cfee7630fa61a166f08
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-13 16:31:21 +0900

    Setup initial grid and flexbox layout

---
+++ game.js
@@ -0,0 +1,8 @@
+// Initial 16x16 Grid
+const container = document.querySelector('.container')
+
+for (let i = 0; i < 256; i++) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    container.appendChild(cell)
+}
