5a99bae7938eb4c7f3f99a61b0d1462fcc295f31
tylersummers <162672490+tylersummers@users.noreply.github.com>
2025-02-27 16:30:16 +0900

    Fix grid size adjustment DOM method

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 // Initial 16x16 Grid
 const container = document.querySelector('.container')
+container.style.display = "grid";
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
+container.style.gridTemplateRows = "repeat(16, 1fr)";
 
 for (let i = 0; i < 256; i++) {
     const cell = document.createElement('div')
@@ -8,9 +11,38 @@ for (let i = 0; i < 256; i++) {
 }
 
 // Colour on Hover Effect
-const cell = document.querySelectorAll('.cell')
-cell.forEach(cell => {
-cell.addEventListener("mouseover", (event) => {
+function hoverEffect() {
+    const cell = document.querySelectorAll('.cell')
+    cell.forEach(cell => {
+    cell.addEventListener("mouseover", (event) => {
+        let colour = Math
         event.target.style.backgroundColor = "blue"
     })
+})
+}
+
+hoverEffect();
+
+// Enable changes to grid size
+const button = document.querySelector('button')
+button.addEventListener("click", (event) => {
+    let gridSize = window.prompt("Enter number of squares per side! (1-100)")
+    gridSize = parseInt(gridSize)
+
+    if (gridSize >= 1 && gridSize <= 100) {
+        const cells = document.querySelector('.container')
+        cells.replaceChildren();
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const cell = document.createElement('div')
+            cell.classList.add('cell')
+            cell.style.width = "100%"
+            cell.style.height = "100%"
+            container.appendChild(cell)
+
+        }
+        hoverEffect();
+    }
 })
